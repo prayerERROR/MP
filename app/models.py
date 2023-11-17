3ad16f84d22f571e8a244e94ccf338ab2a080b32
prayerERROR <qcsdbzzt3@gmail.com>
2023-11-18 02:44:42 +0800

    add register page

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class UserInfo(models.Model):
+    userName = models.CharField(max_length=32)
+    email = models.CharField(max_length=32)
+    mobilePhone = models.CharField(max_length=32)
+    password = models.CharField(max_length=32)
+
--- app/views.py
+++ app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.core.validators import RegexValidator
+from django import forms
+from app import models
 
-# Create your views here.
+
+class RegisterModelForm(forms.ModelForm):
+    userName = forms.CharField(label='User Name',
+                               widget=forms.TextInput(attrs={'class':'form-control'}))
+    email = forms.CharField(label='Email Address',
+                            validators=[RegexValidator(r'[a-zA-Z0-9_+-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}',
+                                                       'Email address format wrong.')],
+                            widget=forms.TextInput(attrs={'class':'form-control'}))
+    mobilePhone = forms.CharField(label='Mobile Phone',
+                                  widget=forms.NumberInput(attrs={'class':'form-control'}))
+    password = forms.CharField(label='Password',
+                               widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    confirmPassword = forms.CharField(label='Confirm Password',
+                                      widget=forms.PasswordInput(attrs={'class':'form-control'}))
+
+    class Meta:
+        model = models.UserInfo
+        fields = '__all__'
+
+
+def register(request):
+    form = RegisterModelForm()
+    return render(request, 'register.html', {'form': form})
